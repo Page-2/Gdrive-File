c8d603c76ae71f2e6ef4488bd4441de55c16b8c5
kolahsiah <77639764+kolahsiah@users.noreply.github.com>
2021-02-09 17:00:22 -0800

    Update worker.js

--- worker/dist/worker.js
+++ worker/dist/worker.js
@@ -3,7 +3,7 @@
 		default_root_id: 'root',
 		client_id: '661552624073-kf541m2fc5on6qls3i3n057i4i3r6pqm.apps.googleusercontent.com',
 		client_secret: 'C1Z_I8UmDe4OACwdvmZGhy30',
-		refresh_token: '1//0dAb0_UwTYWRqCgYIARAAGA0SNwF-L9IreXRHM3C6XdwXIRPf86NSxAgKQPetxNVK8TgYrJ9_2I8bdlo2s9Q55p69sIN_Lo_dVSU',
+		refresh_token: '',
 		service_account: false,
 		service_account_json: {},
 		auth: false,
