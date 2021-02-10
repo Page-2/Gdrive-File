09484054479c89508489c247d0de6527e32c0f0e
kolahsiah <77639764+kolahsiah@users.noreply.github.com>
2021-02-09 17:35:28 -0800

    Update worker.js

--- worker/dist/worker.js
+++ worker/dist/worker.js
@@ -1,5 +1,5 @@
 	self.props = {
-		title: 'Your Title',
+		title: 'GDIndex',
 		default_root_id: 'root',
 		client_id: '202264815644.apps.googleusercontent.com',
 		client_secret: 'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
